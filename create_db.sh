ab08f43d4b6e63d5ebd0b615c1902cdec2dec75a
Carlos Ortigoza Dempster <carlos.otgz@gmail.com>
2016-10-29 11:55:42 -0400

    Changing default DB owner to superuser

--- create_db.sh
+++ create_db.sh
@@ -2,3 +2,4 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$POSTGRES_DB" < /dhcpdb_create.pgsql
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$POSTGRES_DB" -c "ALTER DATABASE ${POSTGRES_DB} OWNER TO ${POSTGRES_USER}"
